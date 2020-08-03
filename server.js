2ce91da85cb35f65e1e37c2352f855902ab3ea69
azagatti <andre.zagatti1@gmail.com>
2020-08-03 00:27:07 -0300

    feat: create a backup and a interval to recover data

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const fs = require("fs");
+
+function recoveryData() {
+  const origin = fs.createReadStream("./backup_db.json", { flags: "r" });
+  const destination = fs.createWriteStream("./db.json", { flags: "w+" });
+  origin.pipe(destination);
+}
+
+setInterval(() => {
+  recoveryData();
+}, 108000);
 
 const port = process.env.PORT || 3333;
 
